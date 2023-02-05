8391d6e015f601a44b3ecbb484ecf50f700f1c58
Adam Simon <adamsimon85100@gmail.com>
2023-02-05 16:56:28 +0200

    adjusted start script

--- run_crawler.sh
+++ run_crawler.sh
@@ -3,10 +3,25 @@
 interval_seconds=$1
 interval_seconds=${interval_seconds:-60}
 
+scrapy_path="undefined"
+scrapy_shared="/usr/local/bin/scrapy"
+scrapy_own="$HOME/.local/bin/scrapy"
+
+if [ -e $scrapy_shared ]
+then
+  scrapy_path=$scrapy_shared
+elif [ -e $scrapy_own ]
+then
+  scrapy_path=$scrapy_own
+else
+  echo "could not find scrapy, exiting..."
+  exit 1
+fi
+
 echo "running crawler every $interval_seconds seconds"
 while true
 do
   echo "running crawler"
-  scrapy crawl water_interruptions
+  $scrapy_path crawl water_interruptions
   sleep $interval_seconds
 done
